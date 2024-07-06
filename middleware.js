50df851a43d5ca0de89f6cc6c42aa8ea585e055e
“Taylor <t4ybennett@gmail.com>
2024-07-06 10:10:24 +0100

    Get basic auth middleware working

--- middleware.js
+++ middleware.js
@@ -1,23 +1,35 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export default withAuth(
-  function middleware(req) {
-    console.log(req.nextUrl.pathname);
-    console.log(req.nextauth.token.role);
+export const config = {
+  matcher: ["/api/:function*", "/Hello"],
+};
 
-    if (req.nextUrl.pathname.startsWith("/CreateUser")) {
-      console.log("We restricted route yo...");
-      return NextResponse.rewrite(new URL("/Denied", req.url));
+const secret = process.env.NEXTAUTH_SECRET;
+
+export default async function middleware(request) {
+  if (
+    request.nextUrl.pathname.startsWith("/api/") &&
+    !request.nextUrl.pathname.startsWith("/api/auth")
+  ) {
+    const token = await getToken({ req: request, secret });
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+    return NextResponse.next();
+  } else {
+    // Use withAuth for other routes specified in the matcher
+    return withAuth(
+      function (request) {
+        // Any specific logic for authenticated routes can go here
+        return NextResponse.next();
+      },
+      {
+        callbacks: {
+          authorized: ({ token }) => !!token,
+        },
+      }
+    )(request);
   }
-);
-
-// TODO TLB: Get authorization to work...
-
-export const config = { matcher: ["/CreateUser"] };
+}
